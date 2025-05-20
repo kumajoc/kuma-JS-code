f0b10d28d5c0545d7604a1f18a253cd87b61461e
kumajoc <kumadata2@gmail.com>
2025-05-20 08:54:15 +0000

    Data type

---
+++ 01_Basic/1variable.js
@@ -0,0 +1,8 @@
+const accountID=123
+let accountEmail="123@gmail.accountEmail"
+var accountPassword="12345"
+accountCity="lalitpur"
+
+console.log(accountID);
+console.table([accountEmail,accountPassword,accountCity])
+
